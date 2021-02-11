11c26501918f129e5552a9f0f21fa92901fe4cc4
4d4143 <manuelalejandrocamino@gmail.com>
2021-02-11 18:44:37 -0300

    Added little example

--- problem1.py
+++ problem1.py
@@ -1,6 +1,7 @@
 # Multiples of 3 and 5 
 # We need to find the numbers that have 3 or (inclusive or..) 5 as factors and then sum them.
 # If 3 or 5 is a factor of X, then X divided by 3 or 5 gives a remainder equal to 0
+# Example: 5 * 3 + 0 = 15 (Where 0 is the remainder)
 
 sumOfNumbers = 0  # For each number we find, we will add it to the sum.
 
