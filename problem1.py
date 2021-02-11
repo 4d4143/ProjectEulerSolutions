08fae36a3f8b60bdcf1b6d1119affa2bef32765e
4d4143 <manuelalejandrocamino@gmail.com>
2021-02-11 18:42:11 -0300

    Below 1000 means up until 999...

--- problem1.py
+++ problem1.py
@@ -2,13 +2,13 @@
 # We need to find the numbers that have 3 or (inclusive or..) 5 as factors and then sum them.
 # If 3 or 5 is a factor of X, then X divided by 3 or 5 gives a remainder equal to 0
 
-sumOfNumbers = 0  # For each number we find, we will add it
+sumOfNumbers = 0  # For each number we find, we will add it to the sum.
 
 
-for number in range(1001):
+for number in range(1000):
     # We loop through all numbers below 1000 and see which ones are divisible by 3 or 5
     if (number % 3 == 0) or (number % 5 == 0):
-        sumOfNumbers += number
+        sumOfNumbers += number 
 
 print(sumOfNumbers)
 
