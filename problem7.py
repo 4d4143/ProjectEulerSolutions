8a5623c1f8ce2da1ae4052f5ff6c0a084d1dc27c
4d4143 <manuelalejandrocamino@gmail.com>
2021-02-23 18:45:17 -0300

    solved problem 7

---
+++ problem7.py
@@ -0,0 +1,19 @@
+# We have to find the 10001th prime number
+# Just that. We have to start at 3 and from there check odd numbers.
+
+def findNthPrime(number):
+    primes = [2]
+    check = 3
+
+    while len(primes) < number: 
+        if all(check % prime != 0 for prime in primes ):
+            primes.append(check) # In case it is, we go to the next prime
+            check += 2
+        else:
+            check += 2 # In case its not we go to the next one
+    
+    return primes[-1] # We return the last element
+
+if __name__ == '__main__':
+    print(findNthPrime(10_001))
+        
